6e50da25c354650b5224bf833912552d765b80c6
muratkucuk <muratkucuk@std.iyte.edu.tr>
2025-05-07 19:32:47 +0300

    update: final version

---
+++ main.py
@@ -0,0 +1,13 @@
+import os
+import random
+import shutil
+
+import numpy as np
+import tensorflow as tf
+import matplotlib.pyplot as plt
+
+from sklearn.model_selection import train_test_split
+from tensorflow.keras import layers, models, optimizers
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+
+
